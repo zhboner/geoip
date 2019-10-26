c004de4ded9ecaa13f785a4823c160005e7de8bb
kslr <kslrwang@gmail.com>
2019-10-27 01:00:08 +0800

    update bin path

--- generate.sh
+++ generate.sh
@@ -10,6 +10,3 @@ ls ./geoip
 
 go get -insecure -u github.com/v2fly/geoip
 $GOPATH/bin/geoip --country=./geoip/GeoLite2-Country-Locations-en.csv --ipv4=./geoip/GeoLite2-Country-Blocks-IPv4.csv --ipv6=./geoip/GeoLite2-Country-Blocks-IPv6.csv
-
-mkdir ./publish
-mv ./geoip.dat ./publish/
